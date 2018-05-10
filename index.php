4a85567defcd68c2883b54ceae705af7c4236907
kimta <kimta@10.0.0.101>
2018-05-10 05:42:58 -0700

    5-10-18 Changes to index.php and styles.css files. Still need to tweak media-queries for hero-banner & header. KL

--- index.php
+++ index.php
@@ -1,39 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 
-
 <head>
-    <title>Acme Assignment</title>
+    <title>PHP Acme Assignment</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Kim Llanos">
     <link rel="stylesheet" href="css/styles.css" type="text/css">
-
 </head>
 
 
 <body>
-    <div id="background">  
-        <!--<img src="/acme/images/site/background.jpg" id="background" alt="Background Image" />-->
-        <div id="container">
-        <header>
-            <?php
-            include $_SERVER['DOCUMENT_ROOT'].'/acme/common/header.php';
-             ?>
-        </header>
+    <div id="background">
+        <div id="container-body">
+            <header>
+                <?php
+                    include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/header.php';
+                    ?>
+            </header>
 
-        <nav>
-            <?php
-            include $_SERVER['DOCUMENT_ROOT'].'/acme/common/nav.php';
-            ?>
-        </nav>
+            <nav>
+                <?php
+                    include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/nav.php';
+                    ?>
+            </nav>
 
-        <main>
-            <h1>Welcome to Acme!</h1>
-            <div class="hero-image">
-                <img class="hero" src="/acme/images/site/rocketfeature.jpg" alt="Rocket Feature" />
-                <div class="flex-container">
+            <main>
+                <h1>Welcome to Acme!</h1>
+                <div id="hero-container">
+                    <div class="hero-banner">
+                        <img class="hero-image" src="/acme/images/site/rocketfeature.jpg" alt="Rocket Feature" />
+                    </div>
                     <div class="hero-text">
-                        <h2 id="herotext">Acme Rocket</h2>
+                        <h2>Acme Rocket</h2>
                         <ul>
                             <li>Quick lightning fuse</li>
                             <li>NHTSA approved seat belts</li>
@@ -42,52 +41,49 @@
                         </ul>
                     </div>
                 </div>
-            </div>
 
-            <section>
-                <div id="featured-recipies">
-                    <h3>Featured Recipes</h3>
-                    <div class="flex-recipe">
-                    <div class="gridImage">
-                        <img src="images/recipes/bbqsand.jpg" alt="BBQ"><br>
-                            <a href="#" title="See Recipe">Pulled Roadrunner BBQ</a>
-                    </div>
-                        <div class="gridImage">
-                            <img src="images/recipes/potpie.jpg" alt="Pot Pie"><br>
-                           <a href="#" title="See Recipe">Roadrunner Pot Pie</a>
-                        </div>
-                       
-                           <div class="gridImage">
-                               <img src="images/recipes/soup.jpg" alt="Soup"><br>
-                            <a href="#" title="See Recipe">Roadrunner Soup</a>
-                           </div>
+                <section>
+                    <div id="featured-recipies">
+                        <h3>Featured Recipes</h3>
+                        <div class="flex-recipe">
+                            <div class="gridImage">
+                                <img src="images/recipes/bbqsand.jpg" alt="BBQ"><br>
+                                <a href="#" title="See Recipe">Pulled Roadrunner BBQ</a>
+                            </div>
+                            <div class="gridImage">
+                                <img src="images/recipes/potpie.jpg" alt="Pot Pie"><br>
+                                <a href="#" title="See Recipe">Roadrunner Pot Pie</a>
+                            </div>
+                            <div class="gridImage">
+                                <img src="images/recipes/soup.jpg" alt="Soup"><br>
+                                <a href="#" title="See Recipe">Roadrunner Soup</a>
+                            </div>
                             <div class="gridImage">
                                 <img src="images/recipes/taco.jpg" alt="Taco"><br>
-                            <a href="#" title="See Recipe">Roadrunner Tacos</a>
+                                <a href="#" title="See Recipe">Roadrunner Tacos</a>
                             </div>
+                        </div>
                     </div>
-                </div>
 
-                <div id="rocket-reviews">
-                    <h3> Acme Rocket Reviews</h3>
-                    <ul>
-                        <li>"I don't know how I ever caught roadrunners before this." (4/5)</li>
-                        <li>"That thing was fast!" (4/5)</li>
-                        <li>"Talk about fast delivery." (5/5)</li>
-                        <li>"I didn't even have to pull the meat apart." (4.5/5)</li>
-                        <li>"I'm on my thirtieth one. I love these things!" (5/5)</li>
-                    </ul>
-                </div>
-            </section>
+                    <div id="rocket-reviews">
+                        <h3>Acme Rocket Reviews</h3>
+                        <ul>
+                            <li>"I don't know how I ever caught roadrunners before this." (4/5)</li>
+                            <li>"That thing was fast!" (4/5)</li>
+                            <li>"Talk about fast delivery." (5/5)</li>
+                            <li>"I didn't even have to pull the meat apart." (4.5/5)</li>
+                            <li>"I'm on my thirtieth one. I love these things!" (5/5)</li>
+                        </ul>
+                    </div>
+                </section>
+            </main>
 
-        </main>
-
-        <footer>
-            <?php
-            include $_SERVER['DOCUMENT_ROOT'].'/acme/common/footer.php';
-            ?>
-        </footer>
-    </div>
+            <footer>
+                <?php
+                    include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/footer.php';
+                    ?>
+            </footer>
+        </div>
     </div>
 </body>
 
